4c294b17f8c5d9ef694d226fff8a90cf08d45abc
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-11-04 18:51:51 +0300

    Complete Unit_ConverterD1 project.

--- Main/TwoWeekJava/Unit_ConverterD1.java
+++ Main/TwoWeekJava/Unit_ConverterD1.java
@@ -9,20 +9,43 @@ public class Unit_ConverterD1 {
         return C;
     }
 
+    public static double MileConverter(double M){
+        double kilometer = M * 1.60934;
+        System.out.println("The length in kilometer is: " + kilometer);
+        return M;
+    }
+
+    public static double InchConverter(double I){
+        double centimeter = I * 2.54;
+        System.out.println("The length in centimeter is: " + centimeter);
+        return I;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("    Which Converter do you want to use? Write the number.");
-        System.out.print("| 1. Celsius To Fahrenheit    2. Mile to Kilometer    3. Inch to Centimeter | \n: ");
+        System.out.println("        Which Converter do you want to use? Write the number.");
+        System.out.println("| 1. Celsius To Fahrenheit    2. Mile to Kilometer    3. Inch to Centimeter |");
         int select = sc.nextInt();
         switch (select){
             case 1:
                 System.out.println("Enter your value(Celsius): ");
-                float celsius = sc.nextInt();
+                float celsius = sc.nextFloat();
                 double result = TemperatureConverter(celsius);
                 break;
-
+            case 2:
+                System.out.println("Enter your value(Mile): ");
+                double Mile = sc.nextDouble();
+                MileConverter(Mile);
+                break;
+            case 3:
+                System.out.println("Enter your value(Inch): ");
+                double Inch = sc.nextDouble();
+                InchConverter(Inch);
+                break;
+            default:
+                System.out.println("Please enter a valid number.");
+                break;
         }
 
 
