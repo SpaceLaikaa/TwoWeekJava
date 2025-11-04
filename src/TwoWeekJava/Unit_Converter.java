20ac7d1202a47bcb3a1e3490d929242c1175c269
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-11-04 15:32:33 +0300

    Removed nested Git repo

---
+++ src/TwoWeekJava/Unit_Converter.java
@@ -0,0 +1,12 @@
+package TwoWeekJava;
+
+public class Unit_Converter {
+    public static double Converter(float n, float m){
+       return n;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
