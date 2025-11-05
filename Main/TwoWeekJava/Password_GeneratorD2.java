9b162f19a3e76707e8d3272d98a1b20e281efc80
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-11-05 10:58:01 +0300

    Finished Day 2 Secure Password Generator.

--- Main/TwoWeekJava/Password_GeneratorD2.java
+++ Main/TwoWeekJava/Password_GeneratorD2.java
@@ -1,18 +1,25 @@
 package TwoWeekJava;
 
 import java.security.SecureRandom;
+import java.util.Scanner;
 
 public class Password_GeneratorD2 {
 
 
     public static void main(String[] args) {
+        int A = 0;
         SecureRandom random = new SecureRandom();
         String pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%";
-
         char[] chars = pool.toCharArray();
 
-        char c = chars[random.nextInt(chars.length)];
-        System.out.println(c);
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("How long do you want your password to be, enter a valid number: ");
+        int length = sc.nextInt();
+
+        for(;A < length;A++) {
+            char c = chars[random.nextInt(chars.length)];
+            System.out.print(c);
+        }
     }
 }
