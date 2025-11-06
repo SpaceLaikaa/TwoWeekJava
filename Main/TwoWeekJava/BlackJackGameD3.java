999f89e1a69b6fe96643f967d21aed7c55709126
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-11-06 19:48:28 +0300

    Progress checkpoint: functionality shaping up, not final yet

--- Main/TwoWeekJava/BlackJackGameD3.java
+++ Main/TwoWeekJava/BlackJackGameD3.java
@@ -3,13 +3,60 @@ package TwoWeekJava;
 import java.util.Random;
 import java.util.Scanner;
 
-public class BlackJackGameD3 {
-    static void main(String[] args) {
-        Random r = new Random(System.currentTimeMillis());
+public class BlackJackGameD3{
+
+    public static int drawCard(Random r) {
+        return r.nextInt(1, 11);
+    }
+
+    public static int total(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Random r = new Random();
         Scanner sc = new Scanner(System.in);
 
-        int black = r.nextInt(1,22);
+        int dealer1 = drawCard(r);
+        int dealer2 = drawCard(r);
 
-        
+        int user1 = drawCard(r);
+        int user2 = 0;
+
+        System.out.println("Croupier: " + dealer1 + " + " + dealer2);
+        System.out.println("-----------------");
+        System.out.println("User: " + user1);
+        System.out.print("Hit or Stand: ");
+        String choice = sc.nextLine();
+
+        if (choice.equalsIgnoreCase("hit")) {
+            user2 = drawCard(r);
+            int userTotal = total(user1, user2);
+            System.out.println("User: " + user1 + " + " + user2 + " = " + userTotal);
+
+            if (userTotal > 21) {
+                System.out.println("You lost :) !");
+            } else if (userTotal == 21) {
+                System.out.println("Blackjack... You win >:( ");
+            } else {
+                System.out.println("You stand with " + userTotal);
+            }
+
+        }
+        else if (choice.equalsIgnoreCase("stand")) {
+            int dealerTotal = total(dealer1, dealer2);
+            System.out.println("User stands with: " + user1);
+            System.out.println("Dealer total: " + dealerTotal);
+
+            if (user1 > dealerTotal) {
+                System.out.println("You win!");
+            } else if (user1 == dealerTotal) {
+                System.out.println("Draw!");
+            } else {
+                System.out.println("You lost!");
+            }
+        } else {
+            System.out.println("Invalid input. Please type 'hit' or 'stand'.");
+        }
     }
 }
