18b9fb2528c83a199c218db1ee14567b2ff57090
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-11-06 14:07:05 +0300

    Started on Day 3, BlackJack Game

---
+++ Main/TwoWeekJava/BlackJackGameD3.java
@@ -0,0 +1,15 @@
+package TwoWeekJava;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class BlackJackGameD3 {
+    static void main(String[] args) {
+        Random r = new Random(System.currentTimeMillis());
+        Scanner sc = new Scanner(System.in);
+
+        int black = r.nextInt(1,22);
+
+        
+    }
+}
--- Main/TwoWeekJava/Password_GeneratorD2.java
+++ Main/TwoWeekJava/Password_GeneratorD2.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Password_GeneratorD2 {
 
-
     public static void main(String[] args) {
         int A = 0;
         SecureRandom random = new SecureRandom();
