9f73b8032068f91e6c9b1da281442b4dc357fd6c
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-11-09 17:52:04 +0300

    Added an unique random lottery numbers generator. Day 5

--- Main/TwoWeekJava/Random_LotteryD5.java
+++ Main/TwoWeekJava/Random_LotteryD5.java
@@ -1,17 +1,35 @@
 package TwoWeekJava;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 
 public class Random_LotteryD5 {
     public static void main(String[] args) {
-        Random r = new Random();
+        Random r = new Random(System.currentTimeMillis());
+        Scanner sc = new Scanner(System.in);
+
         int i = 0;
-        int[] pool = new int[6];
+        int[] lotteryNumbers = new int[6];
+        int[] userGuesses = new int[6];
 
         for(;i<6;i++){
-            pool[i] = r.nextInt(1,50);
+            lotteryNumbers[i] = r.nextInt(1,50);
+            boolean alreadyExists = false;
+            for (int j = 0; j < i; j++) {
+                if (lotteryNumbers[j] == lotteryNumbers[i]) {
+                    alreadyExists = true;
+                    if (alreadyExists) {
+                        i--;
+                    }
+                }
+            }
         }
+
+
+
+        System.out.println(Arrays.toString(lotteryNumbers)); // For checking
     }
 }
