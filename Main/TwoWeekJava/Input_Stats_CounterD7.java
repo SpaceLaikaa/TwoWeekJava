681c2b15d2143bf7f8a1227e59d1e836df5de7da
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-11-11 18:07:50 +0300

    Add program to count positive, negative, and zero values from user input

--- Main/TwoWeekJava/Input_Stats_CounterD7.java
+++ Main/TwoWeekJava/Input_Stats_CounterD7.java
@@ -1,4 +1,32 @@
 package TwoWeekJava;
 
-public class Input_Stats_CounterD7 {
+import java.util.Scanner;
+
+public class Input_Stats_CounterD7{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+
+        int positivenums = 0;
+        int negativenums = 0;
+        int zeros = 0;
+
+        int[] pool = new int[10];
+
+        for(;i<10;i++){
+            System.out.println("Value" + (i+1) + " : ");
+            pool[i] = sc.nextInt();
+            if(pool[i] * -1 < 0 && pool[i] != 0){
+                positivenums++;
+            }
+            else if(pool[i] * -1 > 0 && pool[i] != 0){
+                negativenums++;
+            }
+            else{
+                zeros++;
+            }
+        }
+        System.out.println("There were " + positivenums + " positive, " + negativenums + " negative and, " + zeros + " zero numbers.");
+
+    }
 }
