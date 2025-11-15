b95069465267383bce671514d65ad22321b27ea6
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-11-16 00:17:08 +0300

    Add full 10-question system to Mind Guess Test and complete initial version

--- Main/Week_2/Mind_TestD11.java
+++ Main/Week_2/Mind_TestD11.java
@@ -16,17 +16,49 @@ public class Mind_TestD11 {
         System.out.println("Press ENTER to continue...");
         sc.nextLine();
 
-        // QUESTIONS
+        //ANSWERS
         System.out.println("Do you make decisions quickly? (yes/no)");
         String answer1 = sc.nextLine();
+        if (answer1.equalsIgnoreCase("yes")) riskScore++; else stableScore++;
 
-        if (answer1.equalsIgnoreCase("yes")) {
-            riskScore++;
-        } else if (answer1.equalsIgnoreCase("no")) {
-            stableScore++;
-        }
+        System.out.println("Do you enjoy taking risks? (yes/no)");
+        String answer2 = sc.nextLine();
+        if (answer2.equalsIgnoreCase("yes")) riskScore++; else stableScore++;
 
-        // RESULT
+        System.out.println("Do you prefer to plan everything before acting? (yes/no)");
+        String answer3 = sc.nextLine();
+        if (answer3.equalsIgnoreCase("yes")) stableScore++; else riskScore++;
+
+        System.out.println("Do you try new things often? (yes/no)");
+        String answer4 = sc.nextLine();
+        if (answer4.equalsIgnoreCase("yes")) riskScore++; else stableScore++;
+
+        System.out.println("Do you stay calm under pressure? (yes/no)");
+        String answer5 = sc.nextLine();
+        if (answer5.equalsIgnoreCase("yes")) stableScore++; else riskScore++;
+
+        System.out.println("Do you enjoy surprises? (yes/no)");
+        String answer6 = sc.nextLine();
+        if (answer6.equalsIgnoreCase("yes")) riskScore++; else stableScore++;
+
+        System.out.println("Do you prefer routine over change? (yes/no)");
+        String answer7 = sc.nextLine();
+        if (answer7.equalsIgnoreCase("yes")) stableScore++; else riskScore++;
+
+        System.out.println("Do you get bored easily? (yes/no)");
+        String answer8 = sc.nextLine();
+        if (answer8.equalsIgnoreCase("yes")) riskScore++; else stableScore++;
+
+        System.out.println("Do you think carefully before speaking? (yes/no)");
+        String answer9 = sc.nextLine();
+        if (answer9.equalsIgnoreCase("yes")) stableScore++; else riskScore++;
+
+        System.out.println("Do you like making spontaneous decisions? (yes/no)");
+        String answer10 = sc.nextLine();
+        if (answer10.equalsIgnoreCase("yes")) riskScore++; else stableScore++;
+
+
+        //RESULT
         System.out.println("\n--- RESULT ---");
 
         if (riskScore > stableScore) {
