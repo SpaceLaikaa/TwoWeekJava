5eb6de981ae96899cb07fb56c8ac42248759a44c
Arda <217978919+SpaceLaikaa@users.noreply.github.com>
2025-11-17 00:30:39 +0300

    Implement full player movement logic with counters in maze game

--- Main/Week_2/Maze_BoundD12.java
+++ Main/Week_2/Maze_BoundD12.java
@@ -24,32 +24,50 @@ public class Maze_BoundD12  {
 
         char[][] map = {
                 {'#','#','#','#','#','#','#','#','#','#','#','#','#'},
-                {'#','P',' ',' ',' ','#',' ',' ',' ',' ',' ','M','#'},
-                {'#',' ','#','#','#',' ','#','#','#',' ',' ',' ','#'},
-                {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ','#'},
-                {'#','#','#',' ','#',' ','#','#','#',' ','#',' ','#'},
-                {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ','#'},
+                {'#','P',' ',' ',' ',' ',' ',' ','#',' ',' ','M','#'},
+                {'#',' ','#','#','#',' ','#',' ','#',' ','#',' ','#'},
+                {'#',' ','#',' ',' ',' ','#',' ',' ',' ','#',' ','#'},
+                {'#',' ','#',' ','#','#','#',' ','#','#','#',' ','#'},
+                {'#',' ','#',' ',' ',' ','#',' ','#',' ',' ',' ','#'},
+                {'#',' ','#','#','#',' ',' ',' ','#',' ','#','G','#'},
                 {'#','#','#','#','#','#','#','#','#','#','#','#','#'}
         };
         while(live){
             for (int i = 0;i< map.length;i++){
-                for(int j = 0;j < map.length;j++){
-                    System.out.println(map[i][j]);
+                for(int j = 0;j < map[i].length;j++){
+                    System.out.print(map[i][j]);
                 }
+                System.out.println();
             }
 
             System.out.println("Start moving (w/a/s/d): ");
             String input = sc.nextLine();
 
-            map[playerCol][playerRow] = ' ';
+            map[playerRow][playerCol] = ' ';
 
-            if(input.equalsIgnoreCase("w") && map[playerCol-1][playerRow] != '#'){
-                playerCol--;
+            if(input.equalsIgnoreCase("w") && map[playerRow-1][playerCol] != '#'){
+                playerRow--;
                 upCount++;
             }
+            else if(input.equalsIgnoreCase("a")&& map[playerRow][playerCol-1] != '#') {
+                playerCol--;
+                leftCount++;
+            }
+            else if (input.equalsIgnoreCase("d")&& map[playerRow][playerCol+1] != '#') {
+                playerCol++;
+                rightCount++;
+            }
+            else if (input.equalsIgnoreCase("s")&& map[playerRow+1][playerCol]!='#') {
+                playerRow++;
+                downCount++;
+            }
+            else{
+                System.out.println("Invalid input");
+            }
+
+            map[playerRow][playerCol] = 'P';
         }
         }
 
     }
 
-}
